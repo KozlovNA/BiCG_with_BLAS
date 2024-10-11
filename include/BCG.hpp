19884882d72ed8856d7b42398eb2e365c06a7ec2
Nikolai <kozlov.na@phystech.edu>
2024-10-11 19:38:38 +0300

    BCG added and simplest wxample provided

--- include/BCG.hpp
+++ include/BCG.hpp
@@ -12,10 +12,45 @@ void bcg (const MT  &A,
 
   int n = std::distance(x.begin(), x.end());
 
-  std::vector<T> r0(n);
-  A.template matvec<VT, std::vector<T>>(x, n, r0);
-  cblas_daxpby(n,1,b.data(),1, -1,r0.data(),1);
-  
+  //initializing algorythm 
+  std::vector<T> rk(n);
+  A.template matvec<VT, std::vector<T>>(x, n, rk);
+  cblas_daxpby(n, 1.0,b.data(),1, -1.0,rk.data(),1);
+
+  std::vector<T> pk(n);
+  cblas_dcopy(n, rk.data(),1, pk.data(),1);
+
+  std::vector<T> r0c(n);
+  cblas_dcopy(n, rk.data(),1, r0c.data(),1);
+
+  //variables needed in main loop
+  std::vector<T> vk(n);
+  T alphak;
+  std::vector<T> tk(n);
+  T wk;
+  T betak;
+  T betak_denom;
+  T omegak;
+
+  //main loop
+  for (int k = 0; k < n; k++)
+  {
+    A.template matvec<std::vector<T>, std::vector<T>>(pk, n, vk);
+    betak_denom = cblas_ddot(n, r0c.data(),1, rk.data(),1);
+    alphak = betak_denom /
+             cblas_ddot(n, r0c.data(),1, vk.data(),1);
+    cblas_daxpby(n, -alphak,vk.data(),1, 1.0,rk.data(),1);
+    A.template matvec<std::vector<T>, std::vector<T>>(rk, n, tk);
+    omegak = cblas_ddot(n, tk.data(),1, rk.data(),1) /
+             cblas_ddot(n, tk.data(),1, tk.data(),1);
+    cblas_daxpby(n, alphak,pk.data(),1, 1.0,x.data(),1);
+    cblas_daxpby(n, omegak,rk.data(),1, 1.0,x.data(),1);
+    cblas_daxpby(n, -omegak,tk.data(),1, 1.0,rk.data(),1);
+    betak = cblas_ddot(n, r0c.data(),1, rk.data(),1) * alphak /
+            betak_denom / omegak;
+    cblas_daxpby(n, 1.0,rk.data(),1, betak,pk.data(),1);
+    cblas_daxpby(n, -betak*omegak,vk.data(),1, 1.0,pk.data(),1);
+  }
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,26 @@
 #include<MyMat.hpp>
 int main()
 {
+  //-----------------------------------------------
+  //simplest test on 3x3 matrix
   using VectorType = Eigen::VectorXd;
   using MatrixType = MyMatrixXd;
 
   MatrixType A(3,3);
-  A << 1, 2, 3,
-       4, 5, 6,
-       7, 8, 9;
+  A << 2, 3, 5,
+       3, 7, 4,
+       1, 2, 2;
 
   VectorType b(3);
-  b << 1, 2, 3;
+  b << 10, 3, 3;
 
   VectorType x(3);
-  x << 4, 5, 6;
+  x << 0, 0, 0;
 
   bcg<MatrixType, VectorType>(A, b, x);
-  
+  std::cout << x << "\n"; 
+  //precise solution: 3, -2, 2
+  //-----------------------------------------------
+
   return 0;
 }
